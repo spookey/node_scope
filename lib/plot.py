36dcb73d2792844034de7a0889c918d871ba8e17
Frieder Griesshammer <frieder.griesshammer@der-beweis.de>
2015-08-27 11:54:42 +0200

    output some inter node comparisions; allow numbers in filenames

--- lib/plot.py
+++ lib/plot.py
@@ -1,8 +1,8 @@
 from fnmatch import fnmatch
 from os import listdir, path
-from string import ascii_lowercase
+from string import ascii_lowercase, digits
 
-from pygal import DateTimeLine
+from pygal import DateTimeLine, Treemap
 
 from lib.common import DATADIR, ts_dt
 from lib.files import readjson
@@ -13,7 +13,7 @@ class PlotNode:
         self.hostname = data['hostname']
         self.name = ''.join(
             c for c in self.hostname.lower() if
-            c in (ascii_lowercase + '_' + '-' + '.')
+            c in (ascii_lowercase + digits + '_' + '-')
         )
         self.data = {}
 
@@ -74,8 +74,20 @@ def plot():
             path.join(DATADIR, '{}_{}.svg'.format(name, field))
         )
 
+    def _cmp(comp, node, field):
+        comp.add(node.hostname, [d[-1] for d in node.data[field]])
+        return comp
+
+    compare_clients = Treemap()
+    compare_traffic_rx = Treemap()
+    compare_traffic_tx = Treemap()
+
     for node in _load():
         save(node.name, node.clients(), 'clients')
         save(node.name, node.system(), 'system')
         save(node.name, node.traffic(), 'traffic')
         save(node.name, node.traffic_full(), 'traffic_full')
+
+        save('', _cmp(compare_clients, node, 'clients_total'), 'clients')
+        save('', _cmp(compare_traffic_rx, node, 'traffic_rx'), 'traffic_rx')
+        save('', _cmp(compare_traffic_tx, node, 'traffic_tx'), 'traffic_tx')
